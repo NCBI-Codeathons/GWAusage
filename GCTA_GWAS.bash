2201ef9d55e2ac3da00096534c0056697e99cecb
wis29 <42680939+wis29@users.noreply.github.com>
2020-01-09 15:57:13 -0500

    Updated with basename

--- GCTA_GWAS.bash
+++ GCTA_GWAS.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 OUTPUT_DIR=../../temp
+GENO_BASENAME="genotype_data"
 
 #Perform GWAS
-gcta64 --mlma --bfile ${OUTPUT_DIR}/mydata --grm ${OUTPUT_DIR}/mydata \
+gcta64 --mlma --bfile ${OUTPUT_DIR}/${GENO_BASENAME} --grm ${OUTPUT_DIR}/{GENO_BASENAME} \
         --pheno phenotypes.txt \
         --covar disc_cov.txt \
         --qcovar cont_cov.txt \
-        --out ${OUTPUT_DIR}/mydata \
+        --out ${OUTPUT_DIR}/${GENO_BASENAME} \
         --thread-num 10 --maf 0.01 --autosome
--- GCTA_matrix.bash
+++ GCTA_matrix.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OUTPUT_DIR=../../temp
+GENO_BASENAME="genotype_data"
 
 #Calculate genetic relatedness matrix
-gcta64 --bfile ${OUTPUT_DIR}/mydata --autosome --make-grm --thread-num 10 --out ${OUTPUT_DIR}/mydata
+gcta64 --bfile ${OUTPUT_DIR}/${GENO_BASENAME} --autosome --make-grm --thread-num 10 --out ${OUTPUT_DIR}/${GENO_BASENAME}

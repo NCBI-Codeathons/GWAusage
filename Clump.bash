54fc1e1f7a0db613c3f511cbee0b2ec5a78eaf8c
wis29 <42680939+wis29@users.noreply.github.com>
2020-01-10 12:52:22 -0500

    Add files via upload

---
+++ Clump.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+OUTPUT_DIR=../../../temp
+GENO_BASENAME="genotype_data"
+
+#Generate clump file
+plink --bfile ${OUTPUT_DIR}/${GENO_BASENAME} --clump ${OUTPUT_DIR}/*.mlma --clump-p1 1e-5 --clump-r2 0 --clump-kb 500 --out ${OUTPUT_DIR}/locus --clump-field p --clump-p2 1
+
+#Get snips from clump file
+awk '{print $3}' ${OUTPUT_DIR}/locus.clumped > ${OUTPUT_DIR}/index_snps.txt
